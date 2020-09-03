de99231217b215638dba8c69e21c227c49ad2619
Marcin Walkowski <marcin.walkowski@cognifide.com>
2020-09-03 12:21:14 +0200

    Adding ingress traffic for webserver

--- main.tf
+++ main.tf
@@ -10,8 +10,20 @@ resource "aws_instance" "example" {
               echo "Hello World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
-              
+
   tags = {
       Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "example-sg" {
+  name = "example-sg"
+
+  ingress {
+    from_port    = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]     
+  }
+
 }
