88732c4e19c523c5c13e221c58c2aad3353f4a7e
Marcin Walkowski <marcin.walkowski@cognifide.com>
2020-09-03 12:22:42 +0200

    some allignments

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "example-sg" {
   name = "example-sg"
 
   ingress {
-    from_port    = 8080
+    from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]     
